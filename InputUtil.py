eda08a00519f3374f07e7d8efaf244a1da0e9a79
Daniel Kim <dhjkim2001@gmail.com>
2022-01-27 21:40:48 +0000

    Added class to validate user input

---
+++ InputUtil.py
@@ -0,0 +1,29 @@
+class InputUtil:
+
+
+    def __init__(self):
+        return
+
+    
+    def askToCustomiseBoard(self):
+        answer = input("Would you like to customise your rows and columns?: (Y/N) ")
+        if answer == "Y":
+            return True
+        elif answer == "N":
+            return False
+        else:
+            self.askToCustomiseBoard()
+
+
+    def getRows(self):
+        return int(input("Enter number of rows: "))
+
+
+    def getCols(self):
+        return int(input("Enter number of Columns: "))
+
+
+    def askToSetCell(self):
+        i = int(input("Enter row: "))
+        j = int(input("Enter column: "))
+        return [i, j]
