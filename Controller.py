7c301a96da05e3f26631fafb160ea2ba0c33c273
Daniel Kim <dhjkim2001@gmail.com>
2022-01-27 21:42:50 +0000

    Created method to start the session (Main game loop)

--- Controller.py
+++ Controller.py
@@ -1,13 +1,16 @@
-import BoardModel, BoardView
+import BoardModel, BoardView, InputUtil
 
 class Controller:
-    model = BoardModel.BoardModel()
-    view = BoardView.BoardView()
 
-    def __init__(self, model, view):
+    def __init__(self, model, view, inputUtil):
         self.model = model
         self.view = view
+        self.inputUtil = inputUtil
 
 
     def startSession(self):
         print("Session started!")
+        #customiseBoard = self.inputUtil.askToCustomiseBoard()
+        self.model.initCells()
+        self.view.printBoard(self.model.width, self.model.height, self.model.board)
+        print(self.model.returnDeadNeighbours(0, 0))
