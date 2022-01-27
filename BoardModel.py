9eb13146b2f41aa2eec9baa5dbd4e9ab6f97f8a5
Daniel Kim <dhjkim2001@gmail.com>
2022-01-27 21:42:28 +0000

    Added and changed methods relating to returning living/dead neighbours

--- BoardModel.py
+++ BoardModel.py
@@ -15,11 +15,32 @@ class BoardModel:
             self.board.append(row)
 
     
+    def reviveCell(self, i, j):
+        self.board[i][j] = "0"
+
+
+    def killCell(self, i, j):
+        self.board[i][j] = "."
+
+    
     def returnLivingNeighbours(self, i, j):
         live_neighbours = 0
         for _i in range(i - 1, i + 2):
-            if _i >= 0 or _i < self.height:
+            if _i >= 0 and _i < self.height:
                 for _j in range(j - 1, j + 2):
-                    if self.board[_i][_j] == "0" and (_j >= 0 or _j < self.width):
+                    if self.board[_i][_j] == "0" and (_j >= 0 and _j < self.width):
                         live_neighbours += 1
-        return live_neighbours - 1
+        if live_neighbours == 0:
+            return 0
+        else:
+            return live_neighbours - 1
+
+
+    def returnDeadNeighbours(self, i, j):
+        dead_neighbours = 0
+        for _i in range(i - 1, i + 2):
+            if _i >= 0 and _i < self.height:
+                for _j in range(j - 1, j + 2):
+                    if self.board[_i][_j] == "." and (_j >= 0 and _j < self.width):
+                        dead_neighbours += 1
+        return dead_neighbours - 1
