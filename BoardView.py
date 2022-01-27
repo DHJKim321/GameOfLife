3d6572fa69948ed65f01981c45c3974beda80219
Daniel Kim <dhjkim2001@gmail.com>
2022-01-27 21:41:59 +0000

    Changed BoardView to work with any width, height, and board

--- BoardView.py
+++ BoardView.py
@@ -1,13 +1,15 @@
+import BoardModel
+
 class BoardView:
 
-    
-    def printBoard(self):
-        vertic_divider = "-" * (self.width + 2)
+
+    def printBoard(self, width, height, board):
+        vertic_divider = "-" * (width + 2)
         print(vertic_divider)
-        for i in range(self.height):
+        for i in range(height ):
             curr_row = "|"
-            for j in range(self.width):
-                curr_row += self.board[i][j].draw()
+            for j in range(width):
+                curr_row += board[i][j]
             curr_row += "|"
             print(curr_row)
         print(vertic_divider)
